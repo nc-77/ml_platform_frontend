db4053a98dca72094c1aed7267f2905c0d4b08c8
nc-77 <291993554@qq.com>
2023-03-16 20:59:50 +0800

    添加了DataSet的数据可视化功能

--- src/components/dataSet/DataSet.vue
+++ src/components/dataSet/DataSet.vue
@@ -10,10 +10,22 @@
       </div>
       <template #overlay>
         <a-menu>
-          <a-menu-item @click="showTable" class="my-menu-item">查看数据结果
+          <a-menu-item @click="showTable" class="my-menu-item">
+            <template #icon>
+              <monitor-outlined />
+            </template>
+            查看数据结果
           </a-menu-item>
-          <a-menu-item @click="" class="my-menu-item">数据分布可视化
-          </a-menu-item>
+          <a-sub-menu>
+            <template #icon>
+              <bar-chart-outlined />
+            </template>
+            <template #title><span class="my-menu-item">数据分布可视化</span></template>
+            <a-menu-item v-for="(column) in columns.slice(0,-1)" @click="showPlot(column)" class="my-menu-item">
+              {{ column.title }}
+            </a-menu-item>
+          </a-sub-menu>
+
         </a-menu>
       </template>
     </a-dropdown>
@@ -22,11 +34,20 @@
              wrap-class-name="full-modal">
       <a-table :dataSource="dataSource" :columns="columns"/>
     </a-modal>
+
+    <a-modal v-model:visible="plotVisible" title="数据分布可视化" :footer="null" width="100%"
+             wrap-class-name="full-modal" :destroy-on-close=true>
+      <div id="plotContainer"></div>
+    </a-modal>
+
   </div>
 </template>
 
 <script>
 import * as common from "../common";
+import { nextTick } from 'vue'
+import { Scatter } from '@antv/g2plot';
+import { MonitorOutlined,BarChartOutlined } from "@ant-design/icons-vue";
 
 export default {
   inject: ["getGraph", "getNode"],
@@ -34,6 +55,7 @@ export default {
     return {
       showContextMenu: false,
       tableVisible: false,
+      plotVisible: false,
       logo: "../src/assets/logo.png",
       label: "",
       name: "",
@@ -63,21 +85,47 @@ export default {
       const [file] = node.getData().files.values();
       fetch("http://localhost:8081/files/" + file?.fileId + "/content", {
         method: "GET"
-      }).then(res => res.json()).then(res => this.dataSource = JSON.parse(res.data))
+      }).then(res => res.json()).then(res => {
+        const originData =  JSON.parse(res.data);
+        this.dataSource = originData.map((obj) => {
+          const newObj = {};
+          for (let [key, value] of Object.entries(obj)) {
+            newObj[key] = Number(value);
+          }
+          return newObj;
+        });
+      })
+
     },
     showTable() {
-      if (this.columns.length === 0) {
-        this.getColumns();
-      }
-      if (this.dataSource.length === 0) {
-        this.getDataSources();
-      }
-      console.log(this.columns, this.dataSource);
       this.tableVisible = true;
+    },
+    async showPlot(column) {
+      this.plotVisible = true;
+      await nextTick();
+      const data = this.dataSource;
+      const scatterPlot = new Scatter('plotContainer', {
+        data,
+        xField: column.title,
+        yField: this.columns[this.columns.length-1].title,
+        size: 5,
+        pointStyle: {
+          stroke: '#777777',
+          lineWidth: 1,
+          fill: '#5B8FF9',
+        },
+      });
+      scatterPlot.render();
     }
   },
   mounted() {
     const node = this.getNode();
+    if (this.columns.length === 0) {
+      this.getColumns();
+    }
+    if (this.dataSource.length === 0) {
+      this.getDataSources();
+    }
     // 初始化数据绑定
     common.mapper(node.data, this.$data);
     // 节点数据变化监听，从而绑定数据
@@ -89,7 +137,10 @@ export default {
       },
     })
   },
-
+  components:{
+    MonitorOutlined,
+    BarChartOutlined,
+  },
   computed: {
     nodeClass: function () {
       let clazz = {};
